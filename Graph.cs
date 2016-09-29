b8b04f5f4f077ba29b5c2bc6d6022f761d92cbfc
Tushar Tyagi <tushartyagi@fastmail.com>
2016-09-29 11:24:09 +0530

    Better structure to graphs

--- Graph.cs
+++ Graph.cs
@@ -1,17 +1,119 @@
-public class Vertex {
-    public Boolean Visited {get; set;}
-    public String Name {get; set;}
-}
+using System.Collections.Generic;
 
-public class Edge {
-    public Vertex Vertex1 {get; set;}
-    public Vertex Vertex2 {get; set;}
-}
+namespace tushartyagi.graphs
+{
+    public class Vertex {
 
-public abstract class Graph {
+        public Vertex (string name, bool visited=false) {
+            Name = name;
+            Visited = visited;
+        }
+        public bool Visited {get; set;}
+        public string Name {get; set;}
+    }
 
-}
+    public class Edge {
 
-public class AdjListGraph : Graph {
+        public Edge(Vertex v1, Vertex v2) {
+            Vertex1 = v1;
+            Vertex2 = v2;
+        }
+        public Vertex Vertex1 {get; set;}
+        public Vertex Vertex2 {get; set;}
+    }
 
+    public abstract class Graph {
+        public abstract Graph Add (Vertex v);
+        public abstract Graph Add (Edge e);
+        public abstract bool Contains(Vertex v);
+        public abstract bool Contains(Edge e);
+
+        public abstract IEnumerable<Vertex> GetNeighbours(Vertex v);
+
+    }
+
+    //TODO: Assuming this to be undirected graph, make this generic and 
+    // create a specialised subclass
+    public class AdjListGraph : Graph {
+        private Dictionary<Vertex, List<Vertex>> adjList = new Dictionary<Vertex, List<Vertex>>();
+        override public Graph Add (Vertex v) {
+            adjList.Add(v, new List<Vertex>());
+            return this;
+        }
+
+        override public IEnumerable<Vertex> GetNeighbours(Vertex v) {
+            if (adjList.ContainsKey(v)) {
+                return adjList[v];
+            }
+            else {
+                return new List<Vertex>();
+            }
+        }
+
+        override public Graph Add (Edge e) {
+            // This needs to add both the vertices in the adjList
+            Vertex vertex1 = e.Vertex1,
+                vertex2 = e.Vertex2;
+
+            if (adjList.ContainsKey(vertex1)) {
+                adjList[vertex1].Add(vertex2);
+            } 
+            else {
+                adjList.Add(vertex1, new List<Vertex>{ vertex2 });
+            }
+
+            if (adjList.ContainsKey(vertex2)) {
+                adjList[vertex2].Add(vertex1);
+            } 
+            else {
+                adjList.Add(vertex2, new List<Vertex>{ vertex1 });
+            }
+
+            return this;
+        }
+
+        override public bool Contains(Vertex v) {
+            return adjList.ContainsKey(v);
+        }
+
+        override public bool Contains(Edge e) {
+            if (this.Contains(e.Vertex1)) {
+              List<Vertex> vertices; 
+              var found = adjList.TryGetValue(e.Vertex1, out vertices);
+              if (found) {
+                 return vertices.Contains(e.Vertex2);
+              }
+            } 
+            else if (this.Contains(e.Vertex2)) {
+              List<Vertex> vertices; 
+              var found = adjList.TryGetValue(e.Vertex2, out vertices);
+              if (found) {
+                 return vertices.Contains(e.Vertex1);
+              }
+            }
+            return false;
+        }
+    }
+
+    public class AdjMatrixGraph : Graph {
+        //TODO: Change this to a matrix
+        public Dictionary<Vertex, List<Vertex>> adjList;
+
+        override public IEnumerable<Vertex> GetNeighbours(Vertex v) {
+            return new List<Vertex>();
+        }
+        override public Graph Add (Vertex v) {
+            return this;
+        }
+        override public Graph Add (Edge e) {
+            return this;
+        }
+        override public bool Contains(Vertex v) {
+            return true;
+        }
+
+        override public bool Contains(Edge e) {
+            return true;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace graphs
+namespace tushartyagi.graphs
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Graph g = new AdjListGraph();
+            Vertex a = new Vertex("a"),
+                b = new Vertex("b"),
+                c = new Vertex("c"),
+                d = new Vertex("d"),
+                e = new Vertex("e");
+
+            Edge ab = new Edge(a, b),
+                bc = new Edge(b, c),
+                ad = new Edge(a, d),
+                de = new Edge(d, e),
+                ce = new Edge(c, e);
+
+            g.Add(ab)
+            .Add(bc)
+            .Add(ad)
+            .Add(de)
+            .Add(ce);
+
+            DFS.Search(g, a);
+
+            System.Console.WriteLine("Done");
         }
     }
 }
